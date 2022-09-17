b0c589fa76c33b151ae0f285e317c8e47c16770c
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-09-17 14:35:13 +0300

    Корректировка задачи 4

--- Task-4.py
+++ Task-4.py
@@ -4,18 +4,44 @@ k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0 """
 
 import random
 
-k = int(input("K = "))
-polynomial = ''
-while k >= 0:
-    rand = random.randint(0, 100)
-    if k == 0:
-        polynomial = polynomial + f"{rand} = 0"
-    elif k == 1:
-        polynomial = polynomial + f"{rand}x + "
+# запись в файл
+
+
+def write_file(path, string):
+    with open(path, 'w') as data:
+        data.write(string)
+
+
+def create_mn(k):
+    lst = [random.randint(0, 100) for i in range(k+1)]
+    return lst
+
+
+def create_str(lst_kof):
+    lst = lst_kof[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
     else:
-        polynomial = polynomial + f"{rand}x^{k} + "
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
 
-    k -= 1
 
-with open("file.txt", 'w') as text:
-    text.write(polynomial)
+k = int(input("Введите натуральную степень для первого файла k = "))
+koef1 = create_mn(k)
+write_file("file.txt", create_str(koef1))
+
+with open('file.txt', 'r') as data:
+    print(data.readlines())
